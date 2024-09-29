35f73e0ccfaaf0a2c09b9da1cc52f3453ab85651
pibbling <pibbling@example.com>
2024-09-29 10:37:24 +0100

    remove stretch

--- stretched.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-xrandr --auto --output HDMI-A-0 --mode 1280x1024 --rate 240 --left-of eDP &
-xrandr --output HDMI-A-0 --primary --set "TearFree" on &
-xrandr --output eDP --set "TearFree" on &
