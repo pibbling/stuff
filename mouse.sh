08e240ced794c882f7c55807e24df1a41e1ff3cd
pibbling <pibbling@example.com>
2024-10-14 22:01:54 +0100

    flags sober

---
+++ mouse.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+xinput set-prop 'pointer:Razer Razer Viper V3 Pro' 'libinput Accel Profile Enabled' 0 1 0 &
+xinput set-prop 'pointer:Razer Razer Viper V3 Pro' 'libinput Accel Speed' -0.88 &
+xinput set-prop 'pointer:Razer Viper V3 Pro' 'libinput Accel Profile Enabled' 0 1 0 &
+xinput set-prop 'pointer:Razer Viper V3 Pro' 'libinput Accel Speed' -0.88 &
---
+++ sober/rivals.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp rivals.json /home/nemesis/.var/app/org.vinegarhq.Sober/data/sober/exe/ClientSettings/ClientAppSettings.json
