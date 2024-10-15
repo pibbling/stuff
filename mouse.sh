75e19f69c0a8607c8ec03b58353587b36cd31505
pibbling <pibbling@example.com>
2024-10-15 04:22:14 +0100

    mouse

--- mouse.sh
+++ mouse.sh
@@ -3,3 +3,4 @@ xinput set-prop 'pointer:Razer Razer Viper V3 Pro' 'libinput Accel Profile Enabl
 xinput set-prop 'pointer:Razer Razer Viper V3 Pro' 'libinput Accel Speed' -0.88 &
 xinput set-prop 'pointer:Razer Viper V3 Pro' 'libinput Accel Profile Enabled' 0 1 0 &
 xinput set-prop 'pointer:Razer Viper V3 Pro' 'libinput Accel Speed' -0.88 &
+setxkbmap -layout us
