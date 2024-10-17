1a261312fed5efc0ec573b981ed33a67b29a03a6
pibbling <pibbling@example.com>
2024-10-17 19:47:30 +0100

    fit xrandr

--- xrandr.sh
+++ xrandr.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-xrandr --auto --output HDMI-A-0 --mode 1920x1080 --rate 240 --left-of eDP &
-xrandr --output HDMI-A-0 --primary --set "TearFree" on &
-xrandr --output eDP --set "TearFree" on &
+xrandr --auto --output HDMI-A-1 --mode 1920x1080 --rate 240 --left-of eDP-1-0 &
+xrandr --output eDP-1-0 &
