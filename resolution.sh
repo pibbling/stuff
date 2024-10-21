8fbf2f28ee55567be32f58a789692d72c9c84be6
pibbling <pibbling@example.com>
2024-10-21 13:10:26 +0100

    xin

--- resolution.sh
+++ resolution.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Set the display name explicitly to HDMI-A-0
-DISPLAY_NAME="HDMI-A-0"
+# Set the display name explicitly to HDMI-A-1
+DISPLAY_NAME="HDMI-A-1"
 
 # Function to apply the 1440x1080 @ 240Hz resolution
 apply_custom_resolution() {
@@ -9,7 +9,7 @@ apply_custom_resolution() {
   REFRESH_RATE=$2
 
   # Generate the modeline for 1440x1080 @ 240 Hz with reduced blanking using cvt
-  MODELINE=$(cvt -r ${RESOLUTION%x*} ${RESOLUTION#*x} $REFRESH_RATE | grep Modeline | cut -d' ' -f2-)
+  MODELINE=$(cvt  ${RESOLUTION%x*} ${RESOLUTION#*x} $REFRESH_RATE | grep Modeline | cut -d' ' -f2-)
   
   # Extract mode name from modeline
   MODE_NAME=$(echo $MODELINE | cut -d' ' -f1)
@@ -64,7 +64,7 @@ read -p "Enter 1 for stretch or 2 for native: " choice
 
 if [[ $choice == "1" ]]; then
   # Stretch mode: 1440x1080 @ 240Hz
-  apply_custom_resolution "1440x1080" "240"
+  apply_custom_resolution "1440x1080" "239"
 elif [[ $choice == "2" ]]; then
   # Native mode: 1920x1080 @ 240Hz
   apply_native_resolution
